662cffb1e03ef5c6051932209bf670589235a20c
zhangtao <bloodiron888@gmail.com>
2012-10-19 12:16:07 +0800

    deploy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xushi::Application.config.secret_token = '0670efd5e018e1e2268e41e956d23ef3efaca6011fec31800083854d2314c78599910b6cc12261609c2df672d155c7f4dd8570c062c08b277893c4a6e76be59d'
